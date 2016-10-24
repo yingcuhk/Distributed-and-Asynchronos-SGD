858238c57bf27b3e95e0b4829e249cbe0c0b2091
Ying ZHANG <ying.ie.cuhk@gmail.com>
2016-10-24 11:07:41 +0800

    add noise

--- SGD_LR_oneMachine.py
+++ SGD_LR_oneMachine.py
@@ -5,25 +5,27 @@ from math import sqrt
 
 def SGD():
 
-	M = 30
+	M = 50
 	K = 10
 	N_test = 1000
-	Beta = np.random.randn(M+1,K)
+	Beta_real = np.random.randn(M+1,K)
+	Beta = Beta_real + 0.01*np.random.randn(M+1,K)
+	
+	# test dataset
 	X_test = np.random.randn(N_test,M)
 	add_1s = np.ones((N_test,1))
 	X_test = np.concatenate((X_test,add_1s),axis = 1)
-	temp = np.dot(X_test, Beta) + 0.1 * np.random.randn(N_test,K)
+	temp = np.dot(X_test, Beta) 
 	Y_test = np.argmax(temp, axis = 1) 
-	N_train = 50000
+	
+	# train dataset
+	N_train = 5000
 	X_train= np.random.randn(N_train,M)
 	add_1s = np.ones((N_train,1))
 	X_train = np.concatenate((X_train,add_1s),axis = 1)
-	temp = np.dot(X_train, Beta) + 0.01 * np.random.randn(N_train,K)
+	temp = np.dot(X_train, Beta)
 	Y_train = np.argmax(temp, axis = 1) 
 	
-	y_val = Y_train[0]
-	y_sample = np.zeros(K, dtype = np.float32)
-	y_sample[y_val] = 1
 	graph = tf.Graph()
 	with graph.as_default():
 		beta = tf.placeholder(tf.float32, shape = (M+1,K))# beta is the optimization variable 
@@ -39,19 +41,33 @@ def SGD():
 	with tf.Session(graph = graph) as sess:
 		sess.run(tf.initialize_all_variables())	
 		beta_cur = np.zeros((M+1,K))
-		for t in xrange(N_train):
-			x_sample = X_train[t:t+1,:]
-			y_val = Y_train[t]
-			y_sample = np.zeros((1,K), dtype = np.float32)
-			y_sample[0,y_val] = 1.0
+	
+		errorRate = 0.5	
+
+		L = 5 # size of each batch
+		for k in xrange(N_train*10):
+			t = k % N_train
+			x_sample = X_train[t:t+L,:]
+			y_val = Y_train[t:t+L]
+			y_sample = np.zeros((L,K), dtype = np.float32)
+			print y_val
+			for l, y_val_l in enumerate(y_val):
+				y_sample[l,y_val_l] = 1.0
+			print y_sample
 			Dict = {Train_F:x_sample,beta:beta_cur, Train_L:y_sample}
 			g = sess.run(grad, feed_dict = Dict)
 			g = g[0]
-			beta_cur = beta_cur - 1.0/sqrt(t+1.0) * g
-			Dict = {Train_F:x_sample,beta:beta_cur, Train_L:y_sample}
-			pre_cur = sess.run(predict, Dict)	
+			#stepsize = 1.0/sqrt(k+1.0)
+			#stepsize = min(1.0/(k/10+1.0),0.1)
+			stepsize = 0.001
+			#stepsize = 1.0/sqrt(k+1.0)
+			#stepsize = 1.0/(k+1.0)
+			beta_cur = beta_cur - stepsize * g
 			if t % 100 == 0:
-				print "error rate: ", np.mean(pre_cur != Y_test)
+				Dict = {Train_F:x_sample,beta:beta_cur, Train_L:y_sample}
+				pre_cur = sess.run(predict, Dict)	
+				errorRate = np.mean(pre_cur != Y_test)
+				print "error rate: ", errorRate, "distance to optima: ", np.linalg.norm(beta_cur - Beta_real)
 
 		#print beta_cur
 		#	print type(g)
